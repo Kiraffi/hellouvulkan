03daedd07a8b1c126f396dd4307677f194642a45
Kiraffi <10407365+Kiraffi@users.noreply.github.com>
2022-06-02 14:04:42 +0300

    increase the max amount of allocations for mymemory

--- src/mylibs/container/mymemory.cpp
+++ src/mylibs/container/mymemory.cpp
@@ -19,7 +19,7 @@
 
 
 ///// THIS WILL NOT TO WORK WITH MULTIPLE THREADS!
-static constexpr uint32_t MaxAllocations = 1024u;
+static constexpr uint32_t MaxAllocations = 65536u;
 static constexpr uint32_t MaxMemorySize = 1024u * 1024u * 1024u * 1u;
 static constexpr uint32_t AllocatedSize = MaxMemorySize + 65536u;
 
