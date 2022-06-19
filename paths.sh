0dfac1905b6c8d0231cd758282693dc46213dfe8
Kiraffi <10407365+Kiraffi@users.noreply.github.com>
2022-06-19 05:15:56 +0300

    add i686 to my paths.sh for linux to load the icds

--- paths.sh
+++ paths.sh
@@ -1,2 +1,2 @@
-export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/radeon_icd.x86_64.json:/usr/share/vulkan/icd.d/nvidia_icd.json
+export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/radeon_icd.x86_64.json:/usr/share/vulkan/icd.d/nvidia_icd.json:/usr/share/vulkan/icd.d/radeon_icd.i686.json
 source ~/vulkansdk/1.3.211.0/setup-env.sh
